5edc8b63f05acaf9933ca9106c2f9549dabf59d7
Andrew Blotski <andrew.blotski@globant.com>
2022-02-19 13:15:47 +0300

    prepare deployment

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
-const express = require("express");
-const colors = require("colors");
-const dotenv = require("dotenv").config();
-const { errorHandler } = require("./middleware/errorMiddleware");
-const connectDB = require("./config/db");
+const path = require('path');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -12,8 +13,21 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/goals", require("./routes/goalRoutes"));
-app.use("/api/users", require("./routes/userRoutes"));
+app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  console.log('development!!!!!');
+}
 
 app.use(errorHandler);
 
